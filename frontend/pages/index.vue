12328d4ee929f7476c064f5dd06df58b789a7aba
Asicvfx <asyl252020@mail.ru>
2026-02-25 15:03:56 +0500

    feat: redesigned land(more pink)

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,9 +2,10 @@
   <div class="min-h-screen bg-[#0A0A0B] text-[#EDEDED] font-sans selection:bg-white/20 overflow-x-hidden relative">
     <!-- Background purple glow -->
     <div class="fixed inset-0 pointer-events-none">
-      <div class="absolute top-0 left-1/2 -translate-x-1/2 w-[1200px] h-[800px] bg-[#A855F7]/[0.035] blur-[200px] rounded-full"></div>
-      <div class="absolute bottom-0 right-0 w-[600px] h-[600px] bg-[#7C3AED]/[0.025] blur-[150px] rounded-full"></div>
-      <div class="absolute top-1/2 left-0 w-[400px] h-[400px] bg-[#6366F1]/[0.02] blur-[120px] rounded-full"></div>
+      <div class="absolute top-0 left-1/2 -translate-x-1/2 w-[1400px] h-[900px] bg-[#A855F7]/[0.07] blur-[180px] rounded-full"></div>
+      <div class="absolute bottom-0 right-0 w-[800px] h-[800px] bg-[#7C3AED]/[0.05] blur-[150px] rounded-full"></div>
+      <div class="absolute top-1/3 left-0 w-[600px] h-[600px] bg-[#6366F1]/[0.04] blur-[120px] rounded-full"></div>
+      <div class="absolute bottom-1/3 left-1/3 w-[500px] h-[500px] bg-[#9333EA]/[0.03] blur-[140px] rounded-full"></div>
     </div>
     
     <!-- Navbar -->
